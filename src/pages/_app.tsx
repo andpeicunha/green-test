3e021f70f462005b452dde752fdc8bc83048770e
André Peixoto <andrepeixoto@gmail.com>
2023-03-28 15:06:42 -0300

    first commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,23 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { Hydrate, QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import React from "react";
+import Head from "next/head";
+import GlobalStyle from "@/styles/global";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const [queryClient] = React.useState(() => new QueryClient());
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <>
+          <Head>
+            <title>Green Acesso</title>
+          </Head>
+          <GlobalStyle />
+          <Component {...pageProps} />
+        </>
+      </Hydrate>
+    </QueryClientProvider>
+  );
 }
