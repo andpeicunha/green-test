ed27134391f9284cbd2e9d94ed4bff07c93d0e31
André Peixoto <andrepeixoto@gmail.com>
2023-03-31 09:05:33 -0300

    fix: :bug: Error Styled | Page Without Valid React Component

--- next.config.js
+++ next.config.js
@@ -2,10 +2,12 @@
 
 module.exports = {
   reactStrictMode: true,
+  pageExtensions: ['Styles.tsx', 'Styles.ts', 'Styles.jsx', 'Styles.js'],
   images: {
     domains: ['rickandmortyapi.com', 'localhost'],
   },
   compiler: {
     styledComponents: true,
   },
+
 }
